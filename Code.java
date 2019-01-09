1789182f958f1208400f6fe786553516ec078124
Animesh Dutta <34543420+animesh-dutta@users.noreply.github.com>
2019-01-10 01:50:23 +0530

    Update Code.java

--- Code.java
+++ Code.java
@@ -1,13 +1,12 @@
 /*
-
 Author:Animesh Dutta
 Created On:Dec 30,2017
 File Description:A Sample java file for Linux Terminal
-
 */
 public class Test {
 	public static void main(String args[])
 	{
 	System.out.println("How are you doing?");
+	System.out.println("How much are you earning?");
 	}	
 }
